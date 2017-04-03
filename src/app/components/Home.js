e63432fac1ce6c66698ee9745da5388a0e1def89
shitala-cuelogic <shital.agarwal@cuelogic.co.in>
2017-04-03 18:52:47 +0530

    calling function in react js

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,21 +1,22 @@
 import React from "react";
 
 export class Home extends React.Component {
+
+	constructor(props) {
+		super();
+		this.age = props.age;
+	}
+
+	onMakeOlder() {
+		this.age += 3;
+		console.log(this.age)
+	}
 	render() {
-		console.log(this.props);
 		return(
 			<div>
 				<p>In a new Component!</p>
-				<p>Your name is {this.props.name}, your age is {this.props.age}</p>
-				<p>User Object => name: {this.props.user.name}</p>
-				<div>
-					<h4>Hobbies</h4>
-					<ul>
-						{this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-					</ul>
-				</div>
-				<hr />
-				{this.props.children}
+				<p>Your name is {this.props.name}, your age is {this.age}</p>
+				<button  onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older</button>
 			</div>	
 		);
 	}
@@ -26,6 +27,4 @@ export class Home extends React.Component {
 Home.propTypes = {
 	name: React.PropTypes.string,
 	age: React.PropTypes.number,
-	user: React.PropTypes.object,
-	children: React.PropTypes.element.isRequired
 };
--- src/app/index.js
+++ src/app/index.js
@@ -6,10 +6,6 @@ import {Home} from "./components/Home";
 
 class App extends React.Component {
     render() {
-    	let user = {
-    		name: "Anna",
-    		hobbies: ["Sports", "Reading"]
-    	}
         return (
         	<div className="container">
         		<div className="row">
@@ -20,10 +16,7 @@ class App extends React.Component {
         		<div className="row">
         			<div className="col-xs-10 colxs-offset-1">
         				{/*Attribute can be access useing this.props.attributeName */}
-        				<Home name={"Max"} age={29} user={user}>
-        					{/*This can be access by this.props.children in Home component*/}
-        					<p>This is paragraph</p>
-        				</Home>
+        				<Home name={"Max"} age={29} />
         			</div>
         		</div>
         	</div>
@@ -33,3 +26,5 @@ class App extends React.Component {
 
 
 render(<App/>, window.document.getElementById("app"));
+
+
